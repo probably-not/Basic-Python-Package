d77f02993532a4b6bb7a5b395c3c5ceb6a82d4d3
Coby Benveniste <Coby.Benveniste@gmail.com>
2018-06-26 18:26:55 +0300

    Added an example test using python

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,19 @@
+import unittest
 from context import core
+
+class ExampleTest(unittest.TestCase):
+    """An example test in unittest fashion."""
+    def setUp(self):
+        pass
+
+    def test_will_pass(self):
+        self.assertEqual(1, 1)
+
+    def test_will_not_pass(self):
+        self.assertEqual(1, 1)
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
